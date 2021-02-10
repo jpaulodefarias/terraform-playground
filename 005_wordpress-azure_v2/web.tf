bfef3bc399e041cffba0c58f92fd2ae5a9b45d9a
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-02-10 10:14:19 -0300

    upload project 005

---
+++ 005_wordpress-azure_v2/web.tf
@@ -0,0 +1,119 @@
+resource "azurerm_virtual_network" "wordpress" {
+  name                = "wordpress-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.wordpress.name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "wordpress" {
+  name                 = "wordpress-subnet"
+  resource_group_name  = azurerm_resource_group.wordpress.name
+  virtual_network_name = azurerm_virtual_network.wordpress.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "wordpress" {
+  name                = "wordpress-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.wordpress.name
+  allocation_method   = "Static"
+  domain_name_label   = random_string.fqdn.result
+  tags                = var.tags
+}
+
+resource "azurerm_lb" "wordpress" {
+  name                = "wordpress-lb"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.wordpress.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.wordpress.id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  loadbalancer_id = azurerm_lb.wordpress.id
+  name            = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "wordpress" {
+  resource_group_name = azurerm_resource_group.wordpress.name
+  loadbalancer_id     = azurerm_lb.wordpress.id
+  name                = "ssh-running-probe"
+  port                = var.application_port
+}
+
+resource "azurerm_lb_rule" "lbnatrule" {
+  resource_group_name            = azurerm_resource_group.wordpress.name
+  loadbalancer_id                = azurerm_lb.wordpress.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = var.application_port
+  backend_port                   = var.application_port
+  backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+  frontend_ip_configuration_name = "PublicIPAddress"
+  probe_id                       = azurerm_lb_probe.wordpress.id
+}
+
+resource "azurerm_virtual_machine_scale_set" "wordpress" {
+  name                = "vmscaleset"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.wordpress.name
+  upgrade_policy_mode = "Manual"
+
+  sku {
+    name     = "Standard_DS1_v2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_profile_data_disk {
+    lun           = 0
+    caching       = "ReadWrite"
+    create_option = "Empty"
+    disk_size_gb  = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "vmlab"
+    admin_username       = var.admin_username
+    admin_password       = var.admin_password
+    custom_data          = file("web.conf")
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPConfiguration"
+      subnet_id                              = azurerm_subnet.wordpress.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+      primary                                = true
+    }
+  }
+
+  tags = var.tags
+}
