7a5a4a8486c6148cb9323b5a43c70da256bda0b3
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-02-10 10:16:14 -0300

    update project 005

--- 005_wordpress-azure_v2/output.tf
+++ 005_wordpress-azure_v2/output.tf
@@ -1,7 +1,7 @@
-output "wordpress_public_ip" {
+output "application_public_address" {
   value = azurerm_public_ip.wordpress.fqdn
 }
 
-output "jumpbox_public_ip" {
+output "jumpbox_public_address" {
   value = azurerm_public_ip.jumpbox.fqdn
 }
