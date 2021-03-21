316c3e7abbad127392a2ffa45315eecbd4778eb4
João Paulo de Farias <jpaulodefarias@gmail.com>
2021-03-21 01:32:54 -0300

    update 006

--- 006_aks_v1/main.tf
+++ 006_aks_v1/main.tf
@@ -25,8 +25,6 @@ provider "azuread" {}
 
 provider "kubernetes" {
   host                   = azurerm_kubernetes_cluster.k8s.kube_config.0.host
-  # username               = var.client_id
-  # password               = var.client_secret
   client_key             = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)
   client_certificate     = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)
   cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)
